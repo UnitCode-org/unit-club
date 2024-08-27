3893387afe1efd21477202c4884e123721fd49c4
JJ-unit <justin.jap@unitcode.org>
2024-08-27 14:57:03 +0700

    feat: created fade in component

---
+++ src/components/animations/FadeIn.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import React from 'react';
+import { motion } from "framer-motion";
+
+interface FadeInProps {
+  children: React.ReactNode;
+  delay?: number;
+}
+
+function FadeIn({ children, delay }: FadeInProps) {
+  return (
+    <motion.div
+      className="font-medium text-unit-gray-30"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5, delay: delay ? delay : 0 }}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+export default FadeIn;
