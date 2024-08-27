4a10d5c2072e0c90e814f55b654fc1a04ac5a479
JJ-unit <justin.jap@unitcode.org>
2024-08-27 15:00:12 +0700

    refactor: added duration as fade in param

--- src/components/animations/FadeIn.tsx
+++ src/components/animations/FadeIn.tsx
@@ -6,15 +6,16 @@ import { motion } from "framer-motion";
 interface FadeInProps {
   children: React.ReactNode;
   delay?: number;
+  duration?: number;
 }
 
-function FadeIn({ children, delay }: FadeInProps) {
+function FadeIn({ children, delay, duration }: FadeInProps) {
   return (
     <motion.div
       className="font-medium text-unit-gray-30"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ duration: 0.5, delay: delay ? delay : 0 }}
+      transition={{ duration: duration ? duration : 1, delay: delay ? delay : 0 }}
     >
       {children}
     </motion.div>
