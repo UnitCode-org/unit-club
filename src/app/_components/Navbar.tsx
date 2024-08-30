3b7e11e47e8cb9391aaf8763cff7c9c7ff82bbe0
JJ-unit <justin.jap@unitcode.org>
2024-08-30 10:45:05 +0700

    fix: navbar misaligned on home page

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -100,7 +100,12 @@ function Navbar({ hideLinks }: NavbarProps) {
         )}
       </div>
       {/* Desktop Navbar */}
-      <div className={"hidden md:flex flex-row gap-4 items-center justify-between w-full max-w-screen-2xl py-8 px-10 top-0" + (!isFixed ? " sticky z-10" : " fixed left-0 z-50")}>
+      <div
+        className={
+          "hidden md:flex flex-row gap-4 items-center justify-between w-full py-8 px-10 top-0" +
+          (!isFixed ? " sticky z-10" : " max-w-screen-2xl fixed left-0 z-50")
+        }
+      >
         {!hideLinks && (
           <div className="grow basis-0 flex items-center gap-10">
             <Link
