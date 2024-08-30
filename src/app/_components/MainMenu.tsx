be6e9e9fcec85ffabd09d6e2d72902ab20740ad2
JJ-unit <justin.jap@unitcode.org>
2024-08-30 10:22:37 +0700

    style: made main menu responsive

--- src/app/_components/MainMenu.tsx
+++ src/app/_components/MainMenu.tsx
@@ -44,10 +44,10 @@ function MainMenu() {
           <div className="font-medium">{cursorText}</div>
         </>
       </CustomCursor>
-      <div className="flex w-screen h-screen justify-center items-end gap-8 -mt-[5.75rem]">
+      <div className="flex flex-col md:flex-row w-screen h-screen justify-center items-center md:items-end gap-8 md:-mt-[5.75rem] -mt-[4.75rem] pt-20 pb-8 px-7 md:p-0">
         <Link
           href={"/black"}
-          className="bg-black flex flex-col items-center justify-end rounded-t-full w-1/3 h-2/3 relative overflow-hidden max-w-md group cursor-none"
+          className="bg-black flex flex-col items-center justify-end rounded-t-full w-full md:w-1/3 h-2/3 relative overflow-hidden max-w-md group cursor-none"
           onMouseEnter={() => {
             setCursorVariant(variants.project);
             setCursorText("ENTER");
@@ -60,7 +60,7 @@ function MainMenu() {
           }}
         >
           <div className="absolute bg-gray-600 z-10 w-full h-full opacity-25 group-hover:bg-gray-800 transition-all duration-1000"></div>
-          <div className="absolute h-[60rem] w-[60rem] z-0 -left-80 -top-16 brightness-75 contrast-150 group-hover:brightness-50 transition-all duration-1000">
+          <div className="absolute w-[40rem] h-[40rem] md:h-[60rem] md:w-[60rem] z-0 -left-40 md:-left-80 -top-16 brightness-75 contrast-150 group-hover:brightness-50 transition-all duration-1000">
             <video
               width="2000"
               height="2000"
@@ -76,19 +76,29 @@ function MainMenu() {
           <div
             className={
               "z-10 flex flex-col items-center text-white text-center transition-all duration-1000" +
-              (unitBlackHovered ? " pb-52" : " pb-[0rem] -mb-4")
+              (unitBlackHovered
+                ? " md:pb-52 pb-6"
+                : " md:pb-[0rem] md:-mb-4 pb-6")
             }
           >
             <Image
               src={"/images/logo/unit-black.svg"}
               alt="Unit Black"
+              width={18}
+              height={18}
+              className="block md:hidden"
+            />
+            <Image
+              src={"/images/logo/unit-black.svg"}
+              alt="Unit Black"
               width={28}
               height={28}
+              className="md:block hidden"
             />
-            <h1 className="font-albert-sans font-bold text-5xl mt-3">
+            <h1 className="font-albert-sans font-bold md:text-5xl text-3xl mt-3">
               UNIT BLACK
             </h1>
-            <p className="font-medium mt-4 opacity-0 group-hover:opacity-100 transition-all duration-1000">
+            <p className="font-medium mt-4 opacity-0 group-hover:opacity-100 transition-all duration-1000 hidden md:block">
               {" "}
               YOUR GATEWAY TO EXCLUSIVE
               <br /> EXPERIENCES AND ELITE CONNECTIONS
@@ -97,7 +107,7 @@ function MainMenu() {
         </Link>
         <Link
           href={"/365"}
-          className="bg-black flex flex-col items-center justify-end rounded-t-full w-1/3 h-2/3 relative overflow-hidden max-w-md group cursor-none"
+          className="bg-black flex flex-col items-center justify-end rounded-t-full w-full md:w-1/3 h-2/3 relative overflow-hidden max-w-md group cursor-none"
           onMouseEnter={() => {
             setCursorVariant(variants.project);
             setCursorText("ENTER");
@@ -110,7 +120,7 @@ function MainMenu() {
           }}
         >
           <div className="absolute bg-gray-600 z-10 w-full h-full opacity-25 group-hover:bg-gray-800 transition-all duration-1000"></div>
-          <div className="absolute h-[60rem] w-[60rem] z-0 -left-20 -top-16 brightness-75 contrast-150 group-hover:brightness-50 transition-all duration-1000">
+          <div className="absolute w-[40rem] h-[40rem] md:h-[60rem] md:w-[60rem] z-0 left-0 md:-left-20 -top-16 brightness-75 contrast-150 group-hover:brightness-50 transition-all duration-1000">
             <video
               width="2000"
               height="2000"
@@ -126,19 +136,29 @@ function MainMenu() {
           <div
             className={
               "z-10 flex flex-col items-center text-white text-center transition-all duration-1000" +
-              (unit365Hovered ? " pb-52" : " pb-[0rem] -mb-4")
+              (unit365Hovered
+                ? " md:pb-52 pb-6"
+                : " md:pb-[0rem] md:-mb-4 pb-6")
             }
           >
             <Image
               src={"/images/logo/unit-365.svg"}
               alt="Unit 365"
+              width={32}
+              height={32}
+              className="block md:hidden"
+            />
+            <Image
+              src={"/images/logo/unit-365.svg"}
+              alt="Unit 365"
               width={42}
               height={42}
+              className="md:block hidden"
             />
-            <h1 className="font-albert-sans font-bold text-5xl mt-3">
+            <h1 className="font-albert-sans font-bold md:text-5xl text-3xl mt-3">
               UNIT 365
             </h1>
-            <p className="font-medium mt-4 opacity-0 group-hover:opacity-100 transition-all duration-1000">
+            <p className="font-medium mt-4 opacity-0 group-hover:opacity-100 transition-all duration-1000 hidden md:block">
               {" "}
               YOUR GATEWAY TO EXCLUSIVE
               <br /> EXPERIENCES AND ELITE CONNECTIONS
